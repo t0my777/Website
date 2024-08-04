d2af9d252216cf8899888ed7545f10b5f1296ccd
t0my777 <dark.comercial.pv@gmail.com>
2024-08-03 23:07:10 -0300

    website

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "home": "url('/assets/1c3be5_207508211f624c13a8a31643a18e5660~mv2_d_5472_3648_s_4_2.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
